79a88d4513f2c93f788ef94184a99a0bf6c262c9
Heather Kirk <hskirk@gmail.com>
2019-05-12 18:51:57 +0000

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  
+  get '/signup' do 
+    erb :'/users/signup'
+  end 
+  
+  post '/signup' do
+    @user = User.new(:username => params[:username], :password => params[:password])
+    
+    if user.save && !user,username.empty?
+      redirect '/login'
+    else 
+      redirect '/'
+    end 
+  end 
+  
+  get '/login' do
+    erb :'/users/login'
+  end 
+  
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      redirect '/login'
+    else 
+      redirect '/'
+    end 
+  end 
+  
+  get '/logout' do 
+    session.clear
+    redirect '/'
+  end 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ has_secure_password
 
 helpers do 
   
-  def is_logged_in?
+  def is_logged_in?(session)
     !!session
   end 
   
-  def current_user
-    @session = @user.id 
+  def current_user(session)
+    @user = User.find_by(session[:user_id])
   end 
 end 
